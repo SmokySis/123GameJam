d5967de0e91358e9f1f4753749eb4a27759c1237
Saki-174 <18381601561@163.com>
2026-03-28 20:01:11 +0800

    任务进度读条

---
+++ Assets/_Scripts/MainLogic/ProgressBar.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{    
+    public enum BarMode
+    {
+        AutoFill,         // 纯自动读条
+        QTEDrive,         // QTE驱动（不按不动）
+        QTEOptional       // QTE选做
+    }
+    public Slider slider;          
+    public float totalTime = 20f;      
+    public bool autoStart = true;  
+    
+    public BarMode barMode;
+
+
+    // 当前进度
+    private float currentProgress;
+    private bool isRunning;
+
+
+    void Awake()
+    {
+        if (slider == null)
+            slider = GetComponent<Slider>();
+
+        slider.minValue = 0;
+        slider.maxValue = 1;
+    }
+
+    void Start()
+    {
+        if (autoStart)
+            StartBar();
+    }
+
+    void Update()
+    {
+        if (!isRunning) return;
+        
+        if (barMode == BarMode.AutoFill)
+        {
+            currentProgress += Time.deltaTime / totalTime;
+        }       
+        else if (barMode == BarMode.QTEOptional)
+        {
+            currentProgress += Time.deltaTime / totalTime;
+
+            
+        }
+        else if (barMode == BarMode.QTEDrive)
+        {
+            
+        }
+        // 限制 0~1
+        currentProgress = Mathf.Clamp01(currentProgress);
+        slider.value = currentProgress;
+        // 读条完成
+        if (currentProgress >= 1)
+        {
+            OnBarComplete();
+        }
+    }
+
+    // 开始读条
+    public void StartBar()
+    {
+        isRunning = true;
+        currentProgress = 0;
+        slider.value = 0;
+    }
+
+    // 停止读条
+    public void StopBar()
+    {
+        isRunning = false;
+    }
+
+    // 读条完成
+    void OnBarComplete()
+    {
+        StopBar();
+        Debug.Log("进度条完成！");
+        
+    }
+
+    //外部获取当前进度
+    //可用于QTE弹出时机设置
+    public float GetProgress()
+    {
+        return currentProgress;
+    }
+}
